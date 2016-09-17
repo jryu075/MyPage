c3e7d4ff276b1971e38e3b0d2fb7d32dbcc451c8
John Ryu <ryujungho@hotmail.com>
2016-09-17 06:36:03 +0000

    add mongolab

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+
+mongoose.connect("mongodb://jungho:ryu@ds033126.mlab.com:33126/junghoryu");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
